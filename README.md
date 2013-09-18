e9dec24612150858e72886410407ac21ca15ca40
Andrea Coronese <blackeye84@gmail.com>
2013-09-18 17:07:43 +0200

    first commit WORK IN PROGRESS, not ready yet

--- README.md
+++ README.md
@@ -2,3 +2,50 @@ express-named-router
 ====================
 
 A wrapper for Express JS routes definition which defines named routes
+
+Installation
+--------------
+
+```sh
+npm install express-named-router
+```
+
+Setup
+--------------
+Just initialize the wrapper like this
+
+```sh
+var namedRouter = require('express-named-router');
+
+...
+
+namedRouter.initForApplication(app, {
+    registerAppHelpers: true //Makes named routes accessibles in app.locals
+));
+```
+
+Usage
+------------
+You can still register your routes as usual, but if you want you can
+
+```sh
+app.namedRoute('contacts', {via: 'get', path: '/contact', handler: routes.contact.index, middlewares: []});
+```
+
+In your app.locals, you will find contactsPath = '/contacts', which you can use in your views. For your node scripts, use namedRouter.get('contacts') for the same purpose.
+
+Work in progress
+-----
+Adding parameters mapping to add query string key values to paths at runtime
+
+Status
+---
+UNSTABLE (work in progress)
+
+Notes
+-------------
+express-named-router assumes well formed utf-8 YAML files. Do not mess up.
+
+License
+-----------------
+Released under BSD license.
---
+++ index.js
@@ -0,0 +1,111 @@
+var _ = require('underscore');
+
+//    app.namedRoute('rootPath', {
+//        via: 'get',
+//        path: '/',
+//        middlewares: [...],
+//        handler: function(req,res){...}
+//    });
+
+var namedRouter = function namedRouter() {
+    var _map = {};
+    var applicationInstance = undefined;
+    var _allowedVerbs = ['all', 'get', 'post', 'put', 'del'];
+    var _options = {};
+
+    this.initForApplication = function(appInstance, options) {
+        options = options || {};
+        options.registerAppHelpers = options.registerAppHelpers || true;
+        _options = options;
+        if (typeof appInstance === 'undefined' || !appInstance) {
+            throw new Error('[NamedRouter] applicationInstance option not specified');
+        } else {            
+            applicationInstance = appInstance;
+            appInstance.namedRoute = this.namedRoute;
+        }        
+    };
+
+    this.get = function(name) {
+        return _map[name];
+    };
+
+    this.namedRoute = function(name,data) {
+        var objs;
+        if (typeof name === 'string' && typeof data === 'object') {
+            // single objects passed
+            objs = [];
+
+            var e = {};
+            e[name] = data;
+            objs.push(e);
+
+        } else if (typeof name === 'array') {
+            // Array passed
+            objs = name;
+            //[name: {..data..}];
+        } else {
+            throw new Error('[NamedRouter] namedRoute::SpecifyTheGoddamnParams');
+        }
+        
+        _.each(objs, function(elementInArray) {          
+            // Key is route name, value are options
+            _.map(elementInArray, function(value,key){
+                var routeName = key;
+                var routeData = value;    
+
+                if (typeof routeName === 'undefined' || typeof routeData === 'undefined') {
+                    throw new Error('[NamedRouter] namedRoute::SpecifyTheGoddamnParams');
+                }
+
+                routeData.via = typeof routeData.via !== 'undefined' ? routeData.via.toLowerCase() : 'get';
+                
+                // Check verb consistency
+                if (_.indexOf(_allowedVerbs, routeData.via) === -1) {
+                    throw new Error('[NamedRouter] namedRoute::' + routeData.via + 'VerbNotAllowed');
+                }
+
+                routeData.middlewares = routeData.middlewares || [];
+                
+                routeData.handler = routeData.handler || function(req,res) {
+                    res.send(404);
+                };                
+
+                // Call to expressjs router to add the route
+                applicationInstance[routeData.via](routeData.path, routeData.middlewares, routeData.handler);
+
+                _map[routeName + "Path"] = routeData.path;
+                if (_options.registerAppHelpers === true) {
+                    addNamedRouteToAppLocals(routeName, routeData.path);
+                }
+            }); // map
+            
+        }); // each
+        
+    };
+
+    var addNamedRouteToAppLocals = function(routeName, path) {
+        var installPoint = appHelpersInstallPoint();
+        installPoint[routeName + "Path"] = path;
+    };
+
+    var appHelpersInstallPoint = function() {
+        if (typeof applicationInstance.locals.routes === 'undefined') {
+            applicationInstance.locals.routes = {};
+        }
+
+        return applicationInstance.locals.routes;
+    }
+
+
+}
+
+namedRouter.instance = null;
+namedRouter.getInstance = function() {
+    if (this.instance == null) {
+        this.instance = new namedRouter();
+    }
+
+    return this.instance;
+};
+
+module.exports = exports = namedRouter.getInstance();
